d900712883c17b34acbae3e9243d45ec82a7503d
mrc-augusto <mrc_augusto@hotmail.com>
2023-09-11 17:15:49 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+// let message = alert('Hello World');
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let numA = 10;
+// let numB = 20;
+
+// let sum = alert(Number(numA + numB));
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// let variable = 'mayu';
+
+// typeof variable === 'number' ? alert('é um número') : alert('não é um número');
+
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let variable = 'casa';
+
+// typeof variable === 'string' ? alert('é uma string') : alert('não é uma string');
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano". 
+
+// let variable = false;
+
+// typeof variable === 'boolean' ? alert('é um booleano') : alert('não é um booleano');
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// let numA = 2;
+// let numB = 1;
+
+// let sub = alert(Number(numA - numB));
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// let numA = 5;
+// let numB = 10;
+
+// let mult = alert(Number(numA * numB));
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// let numA = 30;
+// let numB = 15;
+
+// let div = alert(Number(numA / numB));
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// let numberA = 10;
+
+// let total = numberA / 2;
+
+// numberA % 2 == 0 ? alert("Par") : alert("Impar");
